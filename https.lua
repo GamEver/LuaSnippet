02533e893642ea8f0dfc34d1ec3f903aa790b25e
shuax <admin@shuax.com>
2014-11-27 10:59:20 +0800

    还是curl比较方便

--- https.lua
+++ https.lua
@@ -1,40 +1,15 @@
 module('https', package.seeall)
 
-local ffi = require 'ffi'
-
-ffi.cdef[[
-	int curl_global_init(long flags );
-    void *curl_easy_init();
-    int curl_easy_setopt(void *curl, int option, ...);
-    int curl_easy_perform(void *curl);
-    void curl_easy_cleanup(void *curl);
-    char *curl_easy_strerror(int code);
-]]
-
-local libcurl = ffi.load('libcurl')
-assert(libcurl)
-
-local CURLOPT_URL = 10002
-local CURLOPT_WRITEFUNCTION = 20011
-
-local CURLOPT_SSL_VERIFYPEER = 64
-local CURLOPT_SSL_VERIFYHOST = 81
-
 function get(url)
-	local result = nil
-	local function process_data(a,b,c,d)
-		result = ffi.string(a, b*c)
-	end
-	local fptr = ffi.cast("void (*)(char *, size_t, size_t, void *)", process_data)
+	local handle = io.popen("curl -q -k -s -m 1 \""..url.."\"")
+	local result = handle:read("*a")
+	handle:close()
+	return result
+end
 
-	local curl = libcurl.curl_easy_init()
-	if curl then
-		libcurl.curl_easy_setopt(curl, CURLOPT_SSL_VERIFYPEER, 0);
-		libcurl.curl_easy_setopt(curl, CURLOPT_SSL_VERIFYHOST, 0);
-		libcurl.curl_easy_setopt(curl, CURLOPT_URL, url)
-		libcurl.curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, fptr)
-		libcurl.curl_easy_perform(curl)
-		libcurl.curl_easy_cleanup(curl)
-	end
+function post(url, fields)
+	local handle = io.popen("curl -q -k -s -m 1 \""..url.."\" -d \""..fields.."\"")
+	local result = handle:read("*a")
+	handle:close()
 	return result
 end
